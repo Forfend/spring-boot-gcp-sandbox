b4f78a822751cf5dca4359a3067c9fb161da7a54
Oleh Onunfryk <olehonufryk@gmail.com>
2020-09-15 15:17:04 +0300

    volume

--- src/main/java/com/sandbox/springbootgcpsandbox/controllers/GreetingController.java
+++ src/main/java/com/sandbox/springbootgcpsandbox/controllers/GreetingController.java
@@ -1,14 +1,32 @@
 package com.sandbox.springbootgcpsandbox.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class GreetingController {
 
     @GetMapping
-    public String greeting(@RequestParam String name) {
-        return "Hello " + name + "!";
+    public String greeting() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/write")
+    public ResponseEntity<String> writeRandomNumber() {
+        try {
+            String filePath = "/usr/share/tmp/test.txt";
+            Path path = Paths.get(filePath);
+            Files.createFile(path);
+            Files.write(path, "This string has been written".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok("The number has been written!");
     }
 }
