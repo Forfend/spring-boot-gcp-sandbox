4a3efa6ccc1ba57a1bf4090542575c386b6da710
Oleh Onunfryk <olehonufryk@gmail.com>
2021-01-23 21:56:31 +0200

    extended SpringBootServletInitializer

--- src/main/java/com/sandbox/springbootgcpsandbox/SpringBootGcpSandboxApplication.java
+++ src/main/java/com/sandbox/springbootgcpsandbox/SpringBootGcpSandboxApplication.java
@@ -2,12 +2,13 @@ package com.sandbox.springbootgcpsandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootGcpSandboxApplication {
+public class SpringBootGcpSandboxApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGcpSandboxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpSandboxApplication.class, args);
+    }
 
 }
